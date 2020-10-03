f56a8e529f30974accf8d8439efc60306bc91dee
Jay Vishaal J <jayvishaalj14@gmail.com>
2020-10-03 17:32:45 +0530

    Final Web Deploy Version1.0

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,9 +225,27 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("2020/10/10 14:00:00", function (event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
-        });
+        var countDownDate = new Date("Oct 10, 2020 14:00:00").getTime();
+
+        var x = setInterval(function () {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            document.getElementById("month").innerHTML = "0 <span>Months</span>";
+            document.getElementById("days").innerHTML = `${days}<span>Days</span>`;
+            document.getElementById("hours").innerHTML = `${hours}<span>Hours</span>`;
+            document.getElementById("minutes").innerHTML = `${minutes}<span>Minutes</span>`;
+            document.getElementById("seconds").innerHTML = `${seconds}<span>Seconds</span>`;
+
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("parentClock").innerHTML = `<div class="about-content-text mb-80"><h3 class="wow fadeInUp" data-wow-delay="300ms">Event Completed!</h3></div>`;
+            }
+        }, 1000);
     }
 
     // ****************************
